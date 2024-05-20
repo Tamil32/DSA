6d31ff983fdd666e4795fe919c06cd4c2850f649
TAMILSELVAN .M <139545703+Tamil32@users.noreply.github.com>
2024-05-20 23:44:19 +0530

    Create Data Type.java

---
+++ Data Type.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int dataTypeSize(String str) {
+        // code here
+       switch(str){
+           case "Character":
+               return 1;
+           case "Integer":
+               return 4;
+           case "Long":
+               return 8;
+           case "Float":
+               return 4;
+           case "Double":
+               return 8;
+       }
+       return -1;
+        
+    }
+}
